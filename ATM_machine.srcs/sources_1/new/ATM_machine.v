579a7ab6e66d9c146ce92d5d2445d75a5b018b49
takemyt1me <rudwls344@naver.com>
2023-11-28 15:15:33 +0900

    firstcommit

---
+++ ATM_machine.srcs/sources_1/new/ATM_machine.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/28 15:14:15
+// Design Name: 
+// Module Name: ATM_machine
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ATM_machine(
+
+    );
+endmodule
